a61d033a48246faf431d3df951f13e159a83fdbf
SandipOPL <sandip.bhetariya@onlinepsbloans.com>
2022-03-08 15:29:32 +0530

    changes

--- src/main/java/com/alpha/model/CryptoCoinsModel.java
+++ src/main/java/com/alpha/model/CryptoCoinsModel.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Setter
 public @Data class CryptoCoinsModel implements Serializable{
 
+	
 	private String id;
 	
 	private String name;
--- src/main/java/com/alpha/model/DailyPriceTickersModel.java
+++ src/main/java/com/alpha/model/DailyPriceTickersModel.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Setter
 public @Data class DailyPriceTickersModel implements Serializable{
 
+	
 	private String coin_id;
 	
 	private String name;
